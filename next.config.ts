b9ff66223d4d2a76dfbfbd05ee47cca7150ada27
Mike.Yanaranop <52666884+mild2540mike@users.noreply.github.com>
2026-09-10 20:32:36 +0700

    chore: update application version to 1.0.1 and remove unused APP_VERSION variable

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import type { NextConfig } from "next";
+import { version } from "./package.json";
 
 const projectRoot = dirname(fileURLToPath(import.meta.url));
 
 const nextConfig: NextConfig = {
+  env: {
+    NEXT_PUBLIC_APP_VERSION: version,
+  },
   allowedDevOrigins: ["127.0.0.1"],
   devIndicators: false,
   turbopack: {
